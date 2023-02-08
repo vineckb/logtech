c961aef81f9b7cc33cbdd676c67649d8bb7299f7
Hyuri <hyuri.miranda.cortes@gmail.com>
2023-02-08 18:03:59 -0300

    [FABLOG-249] licenca

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { SignIn } from '../pages/SignIn';
  import { Routes,Route } from 'react-router-dom';
 import { Painel } from '../pages/Painel';
 import { Integracao } from '../pages/Integracao';
+import { Configuracao } from '../pages/configuracao';
+import { Licenca } from '../pages/configuracao/licenca';
 
 export default function RoutesApp(){
     return(
@@ -10,6 +12,10 @@ export default function RoutesApp(){
             <Route path="/" element={<SignIn />} />
             <Route path="/painel" element={<Painel />} />
             <Route path="/integracao" element={<Integracao />} />
+            
+            <Route path="/configuracao" element={<Configuracao />} />
+            <Route path="/configuracao/licenca" element={<Licenca />} />
+
             </Routes>
      )
         
