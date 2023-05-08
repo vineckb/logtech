8192f5ff20a0b554de512ef7150f54326fe981c1
Vinicius Borges <vineckb@gmail.com>
2023-05-08 20:31:23 -0300

    feat: create api service with auto token assign

--- src/providers/Auth.tsx
+++ src/providers/Auth.tsx
@@ -1,5 +1,7 @@
 import { AuthContext, User } from '@/contexts/Auth';
-import { ReactElement, useState } from 'react';
+import { api } from '@/services/api';
+import { ReactElement, useEffect, useState } from 'react';
+import { redirect } from 'react-router-dom';
 
 interface AuthProviderProps {
   children?: ReactElement;
@@ -14,6 +16,29 @@ export function AuthProvider({ children }: AuthProviderProps) {
     storedUser ? JSON.parse(storedUser) : ({} as User)
   );
   const [token, setToken] = useState<string>(storedToken || '');
+
+  useEffect(() => {
+    api.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error) {
+        if (401 === error.response.status) {
+          redirect('/signin');
+        } else {
+          return Promise.reject(error);
+        }
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    api.interceptors.request.use(function (config) {
+      const token = localStorage.getItem('token');
+      config.headers.Authorization = token ? `Bearer ${token}` : '';
+      return config;
+    });
+  }, [token]);
 
   function signIn(user: User, token: string) {
     setUser(user);
