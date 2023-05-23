06146c454a17e6cf3c76f2280d1adf0792cca555
Vinicius Borges <vineckb@gmail.com>
2023-05-23 15:00:02 -0300

    refactor(configuracao/permissao-filiais): apply new crud structures

---
+++ src/modules/Configuracao/PermissaoFiliais/settings.ts
@@ -0,0 +1,14 @@
+export const resourceSingularTitle = 'Permissão';
+export const resourcePluralTitle = 'Permissões';
+
+export const resourceName = 'permissao';
+
+export const idKey = 'idpermissao';
+
+export const headers = [
+  { key: 'cnpj', title: 'CNPJ' },
+  { key: 'cliente', title: 'Cliente' },
+  { key: 'qtd_usuarios', title: 'Qtde usuários' },
+  { key: 'chave', title: 'Chave' },
+  { key: 'status', title: 'Status' },
+];
