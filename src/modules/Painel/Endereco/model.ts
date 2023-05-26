f21084a97559a6bde240c541d511bfe459bb8128
Vinicius Borges <vineckb@gmail.com>
2023-05-26 11:17:58 -0300

    fix(painel/endereco): form schema

--- src/modules/Painel/Endereco/model.ts
+++ src/modules/Painel/Endereco/model.ts
@@ -22,14 +22,14 @@ export const schema = z.object({
   idendereco: z.number(),
   rua: z.string().min(1, 'Campo obrigatório'),
   predio: z.string().min(1, 'Campo obrigatório'),
-  andar: z.number(),
-  apto: z.number(),
+  andar: z.string(),
+  apto: z.string(),
   direcao: z.string(),
   altura: z.string(),
   largura: z.string(),
   profundidade: z.string(),
   m3: z.string(),
-  qtdpalete: z.number(),
+  qtdpalete: z.string(),
   idtipoendereco: z.number().min(1, 'Campo obrigatório'),
   idestacao: z.number().min(1, 'Campo obrigatório'),
   iddeposito: z.number().min(1, 'Campo obrigatório'),
@@ -40,20 +40,32 @@ export type FormValues = z.infer<typeof schema>;
 
 export function resourceToFormValues({
   ativo,
+  andar,
+  apto,
+  qtdpalete,
   ...values
 }: Resource): FormValues {
   return {
     ...values,
+    andar: String(andar),
+    apto: String(apto),
+    qtdpalete: String(qtdpalete),
     ativo: ativo === '1',
   };
 }
 
 export function formValuesToResource({
   ativo,
+  andar,
+  apto,
+  qtdpalete,
   ...values
 }: FormValues): Resource {
   return {
     ...values,
+    andar: Number(andar),
+    apto: Number(apto),
+    qtdpalete: Number(qtdpalete),
     ativo: ativo ? '1' : '0',
   };
 }
