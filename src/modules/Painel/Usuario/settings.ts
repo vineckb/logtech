4e66e16a13459458695acc4e026b6a416725a7a3
Vinicius Borges <vineckb@gmail.com>
2023-09-20 16:51:21 -0300

    fix(users): status on listing

--- src/modules/Painel/Usuario/settings.ts
+++ src/modules/Painel/Usuario/settings.ts
@@ -1,15 +1,19 @@
-export const resourceTitle = 'Cadastro de Usuários';
-export const resourceName = 'Usuário';
+export const resourceTitle = "Cadastro de Usuários";
+export const resourceName = "Usuário";
 
-export const resourceKey = 'usuarios';
+export const resourceKey = "usuarios";
 
-export const idKey = 'idusuario';
+export const idKey = "idusuario";
 
 export const headers = [
-  { key: 'nome', title: 'Nome' },
-  { key: 'login', title: 'Login' },
-  { key: 'datacadastro', title: 'Data de cadastro' },
-  { key: 'email', title: 'E-mail' },
-  { key: 'telefone', title: 'Telefone' },
-  { key: 'status', title: 'Status' },
+  { key: "nome", title: "Nome" },
+  { key: "login", title: "Login" },
+  { key: "datacadastro", title: "Data de cadastro" },
+  { key: "email", title: "E-mail" },
+  { key: "telefone", title: "Telefone" },
+  {
+    key: "ativo",
+    title: "Status",
+    filter: (v: number | string) => (v === "s" || v === 1 ? "Sim" : "Não"),
+  },
 ];
