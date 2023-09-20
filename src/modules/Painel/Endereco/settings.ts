7b2f8f0d46199d961642531c6ef66b89fdbd5e19
Vinicius Borges <vineckb@gmail.com>
2023-09-20 16:58:30 -0300

    fix(enderecos): fix table columns

--- src/modules/Painel/Endereco/settings.ts
+++ src/modules/Painel/Endereco/settings.ts
@@ -1,18 +1,22 @@
-export const resourceTitle = 'Cadastro de Endereços';
-export const resourceName = 'Endereço';
+export const resourceTitle = "Cadastro de Endereços";
+export const resourceName = "Endereço";
 
-export const resourceKey = 'enderecos';
+export const resourceKey = "enderecos";
 
-export const idKey = 'idendereco';
+export const idKey = "idendereco";
 
 export const headers = [
-  { key: 'rua', title: 'Rua' },
-  { key: 'predio', title: 'Prédio' },
-  { key: 'andar', title: 'Andar' },
-  { key: 'apto', title: 'Apartamento' },
-  { key: 'direcao', title: 'Direção' },
-  { key: 'tipo', title: 'Tipo' },
-  { key: 'estacao', title: 'Estação' },
-  { key: 'deposito', title: 'Depósito' },
-  { key: 'ativo', title: 'Ativo' },
+  { key: "rua", title: "Rua" },
+  { key: "predio", title: "Prédio" },
+  { key: "andar", title: "Andar" },
+  { key: "apto", title: "Apartamento" },
+  { key: "direcao", title: "Direção" },
+  { key: "tipo", title: "Tipo" },
+  { key: "enderecosestacao.nome", title: "Estação" },
+  { key: "enderecosdeposito.nome", title: "Depósito" },
+  {
+    key: "ativo",
+    title: "Ativo",
+    filter: (v: number | string) => (v === "s" || v === 1 ? "Sim" : "Não"),
+  },
 ];
