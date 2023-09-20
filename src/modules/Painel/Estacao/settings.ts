0bd1d6d526630552ab3c89d85ed3fbe3285bbef7
Vinicius Borges <vineckb@gmail.com>
2023-09-20 17:18:13 -0300

    fix(deposito): table columns

--- src/modules/Painel/Estacao/settings.ts
+++ src/modules/Painel/Estacao/settings.ts
@@ -1,16 +1,20 @@
-export const resourceTitle = 'Cadastro de Estações';
-export const resourceName = 'Estação';
+export const resourceTitle = "Cadastro de Estações";
+export const resourceName = "Estação";
 
 // @todo: review endpoint
-export const resourceKey = 'motivos';
+export const resourceKey = "enderecosestacao";
 
-export const idKey = 'idmotivo';
+export const idKey = "idestacao";
 
 export const headers = [
-  { key: 'nome', title: 'Estação' },
-  { key: 'tiposeparacao', title: 'Tipo Separação' },
-  { key: 'tipo', title: 'Tipo Estação' },
-  { key: 'ordenacao', title: 'Ordenação' },
-  { key: 'deposito', title: 'Depósito' },
-  { key: 'status', title: 'Status' },
+  { key: "nome", title: "Estação" },
+  { key: "tiposeparacao", title: "Tipo Separação" },
+  { key: "tipoestacao.nome", title: "Tipo Estação" },
+  { key: "ordenacao", title: "Ordenação" },
+  { key: "enderecosdeposito.nome", title: "Depósito" },
+  {
+    key: "ativo",
+    title: "Ativo",
+    filter: (v: number | string) => (v === "s" || v === 1 ? "Sim" : "Não"),
+  },
 ];
